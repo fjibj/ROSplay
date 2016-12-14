986d0fdb9e2cadd06acdcd6d172a260eedbe8094
方进 <fjibj@hotmail.com>
2016-12-14 23:01:55 +0800

    将“IP摄像头”换成“DroidCam”
    由于opencv采集“IP摄像头”图像时经常报错，故更新成DroidCam摄像头

--- testCam.py
+++ testCam.py
@@ -3,8 +3,8 @@ import cv2
 import time
 if __name__ == '__main__':   
 	cv2.namedWindow("camera",1)  
-	#开启ip摄像头  
-	video="http://admin:admin@192.168.1.101:8081/"  
+	#开启DroidCam手机摄像头   
+	video="http://192.168.1.101:4747/mjpegfeed?.mjpg"  
 	cap = cv2.VideoCapture(video)
 	num = 0;  
 	while(cap.isOpened()):
